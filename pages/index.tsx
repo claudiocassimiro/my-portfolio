cfbff949d97b5be82dec7ef3acaf7ea7de8b02fa
claudiocassimiro <days_surfing@live.com>
2023-03-02 11:27:03 -0300

    feat: remove image element and add ClaudioLogo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,12 @@
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useWindowSize } from '../utils/helpers'
 import SearchInput from '../components/SearchInput'
+import ClaudioLogo from '../components/ClaudioLogo'
 
 export default function Home() {
-  const { isDesktop } = useWindowSize()
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Image
-          className={styles.image}
-          src="/images/claudio-icon.webp"
-          alt="Claudio icon"
-          width={isDesktop ? 417 : 160}
-          height={isDesktop ? 113 : 56}
-        />
-
+        <ClaudioLogo />
         <SearchInput />
 
         <div className={styles.containerfakeButtons}>
