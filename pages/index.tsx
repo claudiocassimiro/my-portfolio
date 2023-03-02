de5ee53f5a408c14fc3d0886c3ced23ad1cb6127
claudiocassimiro <days_surfing@live.com>
2023-03-02 13:27:45 -0300

    feat: add MoreLinks component to initial page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import styles from '../styles/Home.module.css'
 import SearchInput from '../components/SearchInput'
 import ClaudioLogo from '../components/ClaudioLogo'
+import MoreLinks from '../components/MoreLinks'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <div className={styles.containerMoreLinks}>
+          <MoreLinks />
+        </div>
         <ClaudioLogo />
         <SearchInput />
 
