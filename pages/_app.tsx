61fea230599dcd09f6bca982c5ca508f4338f639
claudiocassimiro <days_surfing@live.com>
2023-03-02 10:41:44 -0300

    feat: config page metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Claudio Cassimiro - Software Engineer</title>
+        <meta
+          name="description"
+          content="Software Engineer - React, NextJS, React Native, Type-Script, NestJS, NodeJS, Front-End Engineer, Back-End Engineer, Mobile Developer"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
